3eab188aff6f7c735a41b7637ae8c4c3e4c1c177
Beaksooyeol <merrygoaround0726@gmail.com>
2021-01-15 18:48:34 +0900

    홈 메인 만들고, 여자친구랑 헤어짐.

---
+++ app/src/main/java/org/techtown/study01/FirstToMain/findid/Id_pw_complete.java
@@ -0,0 +1,17 @@
+package org.techtown.study01.FirstToMain.findid;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.techtown.study01.FirstToMain.R;
+
+public class Id_pw_complete extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.id_pw_complete);
+    }
+}
