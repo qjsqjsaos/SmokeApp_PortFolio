c011ac6d130b0294305dffb68e3c02d04d05ef67
Beaksooyeol <merrygoaround0726@gmail.com>
2021-01-19 18:48:18 +0900

    홈 메인 리니어로 바꾸기

--- app/src/main/java/org/techtown/study01/FirstToMain/homeMain/BottomNavi.java
+++ app/src/main/java/org/techtown/study01/FirstToMain/homeMain/BottomNavi.java
@@ -1,9 +1,11 @@
-package org.techtown.study01.FirstToMain.homeMain;
+    package org.techtown.study01.FirstToMain.homeMain;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -13,20 +15,41 @@ import androidx.navigation.ui.NavigationUI;
 import org.techtown.study01.FirstToMain.R;
 
 public class BottomNavi extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+    HomeMain fragment1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottom_navi);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragm);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+
+        bottomNavigationView = findViewById(R.id.nav_view);
+
+        /**프래그먼트 생성*/
+        fragment1 = new HomeMain();
+
+        /**제일 처음 띄워주는 프래그먼트*/
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,fragment1).commitAllowingStateLoss();
+
+        /**아이콘 선택시 원하는 프래그먼트가 띄워지게 하기*/
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                /**각 탭에 아이디의 따라 다르게 띄워주기*/
+                switch (item.getItemId()) {
+                    case R.id.home : {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment1).commitAllowingStateLoss();
+
+                        return true;
+                    }
+
+                    default:return false;
+                }
+            }
+        });
+
+
     }
+
 
 }
--- app/src/main/java/org/techtown/study01/FirstToMain/homeMain/HomeMain.java
+++ app/src/main/java/org/techtown/study01/FirstToMain/homeMain/HomeMain.java
@@ -1,17 +1,26 @@
 package org.techtown.study01.FirstToMain.homeMain;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import org.techtown.study01.FirstToMain.R;
 
 //홈 화면
-public class HomeMain extends AppCompatActivity {
+public class HomeMain extends Fragment {
+
+    ViewGroup viewGroup;
+
+    @Nullable
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.home_main);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        viewGroup = (ViewGroup) inflater.inflate(R.layout.home_main, container, false);
+        return viewGroup;
     }
+
 }
