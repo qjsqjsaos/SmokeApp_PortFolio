bf0af0a8c00b83eed66dc25999ebcccaf9e074f6
Beaksooyeol <merrygoaround0726@gmail.com>
2021-01-29 16:11:19 +0900

    모든 오류 try catch문 익히고 처리, 이제 타이머만 설정하면 로그인 부분 완료

--- app/src/main/java/org/techtown/study01/FirstToMain/register/Register.java
+++ app/src/main/java/org/techtown/study01/FirstToMain/register/Register.java
@@ -51,9 +51,10 @@ public class Register extends AppCompatActivity {
     private Button sendEmail, email_btn = null;
     private EditText email, smsNumber = null; //받는 사람의 이메일
     private int result; //이메일 인증번호
-    public int keyNumber; //입력한 인증번호
+    private int keyNumber; //입력한 인증번호
     private TextView countView;
-    
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -78,24 +79,27 @@ public class Register extends AppCompatActivity {
                 AlertDialog.Builder builder = new AlertDialog.Builder(Register.this);
 
 
-
-                //입력칸이 빈칸일때
-                if (id.equals("")) {
-                    dialog = builder.setMessage("아이디를 입력해주세요.")
-                            .setPositiveButton("확인", null)
-                            .create();
-                    dialog.show();
-                    return;
+                try {
+                    //입력칸이 빈칸일때
+                    if (id.equals("")) {
+                        dialog = builder.setMessage("아이디를 입력해주세요.")
+                                .setPositiveButton("확인", null)
+                                .create();
+                        dialog.show();
+                        return;
+                    }
+                    //아이디 유효성
+                    if (!Pattern.matches("^[0-9_a-zA-Z]{4,20}$", id)) {
+                        dialog = builder.setMessage("아이디 형식을 지켜주세요.")
+                                .setPositiveButton("확인", null)
+                                .create();
+                        dialog.show();
+                        return;
+                    }
+                }catch (Exception e){
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(),"잘못된 값입니다. 문의 부탁드립니다.",Toast.LENGTH_SHORT).show();
                 }
-                //아이디 유효성
-                if(!Pattern.matches("^[0-9_a-zA-Z]{4,20}$", id)) {
-                    dialog = builder.setMessage("아이디 형식을 지켜주세요.")
-                            .setPositiveButton("확인", null)
-                            .create();
-                    dialog.show();
-                    return;
-                }
-
 
 
                 Response.Listener<String> responseListener=new Response.Listener<String>() { //결과 리스너 생성 (중복체크)
@@ -136,8 +140,6 @@ public class Register extends AppCompatActivity {
         });
 
 
-
-
         //이메일 인증번호 보내기
         StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
                 .permitDiskReads()
@@ -145,13 +147,8 @@ public class Register extends AppCompatActivity {
                 .permitNetwork().build());
 
         email = (EditText) findViewById(R.id.email); //받는 사람의 이메일
-
-
         sendEmail = findViewById(R.id.sendEmail);
         sendEmail.setOnClickListener(new View.OnClickListener() {
-
-
-
             @Override
             public void onClick(View v) {
 
@@ -167,12 +164,14 @@ public class Register extends AppCompatActivity {
                     gMailSender.sendMail("금연투게더 인증번호 입니다.", "인증번호는 : " + result +" 입니다. \n " +
                             "인증번호를 입력하시고 확인버튼을 누르시면 회원가입이 완료됩니다.", email.getText().toString());
                     Toast.makeText(getApplicationContext(), "인증번호가 전송되었습니다.", Toast.LENGTH_SHORT).show();
+                    return;
                 } catch (SendFailedException e) {
                     Toast.makeText(getApplicationContext(), "이메일 형식이 잘못되었습니다.", Toast.LENGTH_SHORT).show();
                 } catch (MessagingException e) {
                     Toast.makeText(getApplicationContext(), "이메일을 입력해주세요", Toast.LENGTH_SHORT).show();
-                } catch (Exception e) {
+                } catch (Exception e){
                     e.printStackTrace();
+                    Toast.makeText(getApplicationContext(),"잘못된 값입니다. 문의 부탁드립니다.",Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -180,39 +179,43 @@ public class Register extends AppCompatActivity {
                 //이메일 인증버튼 눌렀을 때 반응
 
                 smsNumber = (EditText) findViewById(R.id.smsNumber);
-
                 email_btn = (Button) findViewById(R.id.email_btn);
                 email_btn.setOnClickListener(new View.OnClickListener() {
-
                     @Override
                     public void onClick(View v) {
-                        keyNumber = Integer.parseInt(smsNumber.getText().toString());
                         AlertDialog.Builder builder = new AlertDialog.Builder(Register.this);
-
-                        if (smsNumber.length() == 0) {
-                            dialog = builder.setMessage("인증번호를 입력해주세요.")
-                                    .setPositiveButton("확인", null)
+                        try {
+                            keyNumber = Integer.parseInt(smsNumber.getText().toString());
+                            if (smsNumber.length() == 0) {
+                                dialog = builder.setMessage("인증번호를 입력해주세요.")
+                                        .setPositiveButton("확인", null)
+                                        .create();
+                                dialog.show();
+                                return;
+                            } else if (result == keyNumber) {
+                                dialog = builder.setMessage("인증 되었습니다.")
+                                        .setPositiveButton("확인", null)
+                                        .create();
+                                dialog.show();
+                                return;
+                            }else if (keyNumber < 1000000 && result != keyNumber) {
+                                dialog = builder.setMessage("인증번호가 틀립니다.")
+                                        .setNegativeButton("확인", null)
+                                        .create();
+                                dialog.show();
+                                return;
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            dialog = builder.setMessage("인증번호를 확인해주세요.")
+                                    .setNegativeButton("확인", null)
                                     .create();
                             dialog.show();
                             return;
                         }
 
-                        else if(result == keyNumber){
-                            dialog=builder.setMessage("인증 되었습니다.")
-                                    .setPositiveButton("확인",null)
-                                    .create();
-                            dialog.show();
-                            return;
-                        }
-                        else if(result != keyNumber){
-                            dialog=builder.setMessage("인증번호가 틀립니다.")
-                                    .setNegativeButton("확인",null)
-                                    .create();
-                            dialog.show();
-                            return;
-                        }
                     }
-        });
+                });
 
 
 
@@ -278,77 +281,82 @@ public class Register extends AppCompatActivity {
 
 
                     //각 정보를 입력안했을때는 Toast메세지 출력 후 리턴
-                    if (isEmpty(id)) {
-                        Toast.makeText(getApplicationContext(), "아이디를 입력해주세요.", Toast.LENGTH_LONG).show();
-                        return;
-                    } else if (validate == null) {
-                        Toast.makeText(getApplicationContext(), "아이디 중복확인을 해주세요.", Toast.LENGTH_LONG).show();
-                        return;
-                    } else if (isEmpty(pw)) {
-                        Toast.makeText(getApplicationContext(), "비밀번호를 입력해주세요.", Toast.LENGTH_LONG).show();
-                        return;
-                    } else if (isEmpty(pwc)) {
-                        Toast.makeText(getApplicationContext(), "비밀번호를 확인하세요.", Toast.LENGTH_LONG).show();
-                        return;
-                    } else if (!pw.equals(pwc)) {
-                        Toast.makeText(getApplicationContext(), "비밀번호가 일치하지 않습니다.", Toast.LENGTH_LONG).show();
-                        return;
-                    } else if (isEmpty(name)) {
-                        Toast.makeText(getApplicationContext(), "닉네임을 입력해주세요.", Toast.LENGTH_LONG).show();
-                        return;
-                    } else if (isEmpty(email)) {
-                        Toast.makeText(getApplicationContext(), "이메일을 입력해주세요.", Toast.LENGTH_LONG).show();
-                        return;
-                    } else if (isEmpty(smsnumber)) {
-                        Toast.makeText(getApplicationContext(), "인증번호를 입력해주세요.", Toast.LENGTH_LONG).show();
-                        return;
-                    } else if (check_number_smtp == null) {
-                        Toast.makeText(getApplicationContext(), "인증번호 확인버튼을 눌러주세요.", Toast.LENGTH_LONG).show();
-                        return;
-                    } else if (result == keyNumber) {
-                        Toast.makeText(getApplicationContext(), "인증번호가 일치하지않습니다.", Toast.LENGTH_LONG).show();
-                        return;
-                    } else if(!checkId.equals(id)) {
-                        Toast.makeText(getApplicationContext(), "아이디 중복확인을 눌러주세요.", Toast.LENGTH_LONG).show();
-                        return;
-                    }
+                   try {
+                       if (isEmpty(id)) {
+                           Toast.makeText(getApplicationContext(), "아이디를 입력해주세요.", Toast.LENGTH_LONG).show();
+                           return;
+                       } else if (validate == null) {
+                           Toast.makeText(getApplicationContext(), "아이디 중복확인을 해주세요.", Toast.LENGTH_LONG).show();
+                           return;
+                       } else if (isEmpty(pw)) {
+                           Toast.makeText(getApplicationContext(), "비밀번호를 입력해주세요.", Toast.LENGTH_LONG).show();
+                           return;
+                       } else if (isEmpty(pwc)) {
+                           Toast.makeText(getApplicationContext(), "비밀번호를 확인하세요.", Toast.LENGTH_LONG).show();
+                           return;
+                       } else if (!pw.equals(pwc)) {
+                           Toast.makeText(getApplicationContext(), "비밀번호가 일치하지 않습니다.", Toast.LENGTH_LONG).show();
+                           return;
+                       } else if (isEmpty(name)) {
+                           Toast.makeText(getApplicationContext(), "닉네임을 입력해주세요.", Toast.LENGTH_LONG).show();
+                           return;
+                       } else if (isEmpty(email)) {
+                           Toast.makeText(getApplicationContext(), "이메일을 입력해주세요.", Toast.LENGTH_LONG).show();
+                           return;
+                       } else if (isEmpty(smsnumber)) {
+                           Toast.makeText(getApplicationContext(), "인증번호를 입력해주세요.", Toast.LENGTH_LONG).show();
+                           return;
+                       } else if (check_number_smtp == null) {
+                           Toast.makeText(getApplicationContext(), "인증번호 확인버튼을 눌러주세요.", Toast.LENGTH_LONG).show();
+                           return;
+                       } else if (result != keyNumber) {
+                           Toast.makeText(getApplicationContext(), "인증번호가 일치하지않습니다.", Toast.LENGTH_LONG).show();
+                           return;
+                       } else if (!checkId.equals(id)) {
+                           Toast.makeText(getApplicationContext(), "아이디 중복확인을 눌러주세요.", Toast.LENGTH_LONG).show();
+                           return;
+                       }
 
-                    //비밀번호 유효성
-                    else if (!Pattern.matches("^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!%*#?&]).{8,20}.$", pw)) {
-                        AlertDialog.Builder builder = new AlertDialog.Builder(Register.this);
-                        dialog = builder.setMessage("비밀번호 형식을 지켜주세요.")
-                                .setPositiveButton("확인", null)
-                                .create();
-                        dialog.show();
-                        return;
-                    }
+                       //비밀번호 유효성
+                       else if (!Pattern.matches("^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!%*#?&]).{8,20}.$", pw)) {
+                           AlertDialog.Builder builder = new AlertDialog.Builder(Register.this);
+                           dialog = builder.setMessage("비밀번호 형식을 지켜주세요.")
+                                   .setPositiveButton("확인", null)
+                                   .create();
+                           dialog.show();
+                           return;
+                       }
 
-                    //이메일 형식체크
-                    else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-                        AlertDialog.Builder builder = new AlertDialog.Builder(Register.this);
-                        dialog = builder.setMessage("이메일 형식을 지켜주세요.")
-                                .setPositiveButton("확인", null)
-                                .create();
-                        dialog.show();
-                        return;
-                    }
+                       //이메일 형식체크
+                       else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                           AlertDialog.Builder builder = new AlertDialog.Builder(Register.this);
+                           dialog = builder.setMessage("이메일 형식을 지켜주세요.")
+                                   .setPositiveButton("확인", null)
+                                   .create();
+                           dialog.show();
+                           return;
+                       }
 
 
-                    //닉네임 유효성
-                    else if (!Pattern.matches("^[가-힣a-zA-Z0-9_]{2,15}$", name)) {
-                        AlertDialog.Builder builder = new AlertDialog.Builder(Register.this);
-                        dialog = builder.setMessage("닉네임 형식을 지켜주세요.")
-                                .setPositiveButton("확인", null)
-                                .create();
-                        dialog.show();
-                        return;
-                    } else {
-                        //모든 값이 다 있으면 DB에 저장하는 메소드 실행
-                        RegisterRequest register = new RegisterRequest(id, pw, name, email, responseListener);
-                        RequestQueue queue = Volley.newRequestQueue(Register.this);
-                        queue.add(register);
-                    }
-                        return;
+                       //닉네임 유효성
+                       else if (!Pattern.matches("^[가-힣a-zA-Z0-9_]{2,15}$", name)) {
+                           AlertDialog.Builder builder = new AlertDialog.Builder(Register.this);
+                           dialog = builder.setMessage("닉네임 형식을 지켜주세요.")
+                                   .setPositiveButton("확인", null)
+                                   .create();
+                           dialog.show();
+                           return;
+                       } else {
+                           //모든 값이 다 있으면 DB에 저장하는 메소드 실행
+                           RegisterRequest register = new RegisterRequest(id, pw, name, email, responseListener);
+                           RequestQueue queue = Volley.newRequestQueue(Register.this);
+                           queue.add(register);
+                       }
+                       return;
+                   } catch (Exception e){
+                       e.printStackTrace();
+                       Toast.makeText(getApplicationContext(),"잘못된 값입니다. 문의 부탁드립니다.",Toast.LENGTH_SHORT).show();
+                   }
             }
         });
     }
