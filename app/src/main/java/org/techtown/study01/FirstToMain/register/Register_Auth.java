5e39eb02b4d78074ce6fdfc738e3918ec357ff21
Beaksooyeol <merrygoaround0726@gmail.com>
2021-02-02 23:36:40 +0900

    체크박스 오류 마지막 성공

--- app/src/main/java/org/techtown/study01/FirstToMain/register/Register_Auth.java
+++ app/src/main/java/org/techtown/study01/FirstToMain/register/Register_Auth.java
@@ -30,28 +30,37 @@ public class Register_Auth extends AppCompatActivity{
                         @Override
                         public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                                 boolean checked = buttonView.isChecked();
-                                if(checked){
-                                        if(!check1.isChecked() && !check2.isChecked()) {
-                                                check1.setChecked(true);
-                                                check2.setChecked(true);
-                                        }
-                                        button.setOnClickListener(new View.OnClickListener() {
-                                                @Override
-                                                public void onClick(View v) {
-                                                        if(check1.isChecked() && check2.isChecked()){
-                                                                Intent intent = new Intent(Register_Auth.this, Register.class);
-                                                                startActivity(intent);
-                                                        }else if(check1.isChecked() && check2.isChecked() && checkBoxAll.isChecked()){
-                                                                Intent intent = new Intent(Register_Auth.this, Register.class);
-                                                                startActivity(intent);
-                                                        }else{
-                                                                Toast.makeText(getApplicationContext(), "필수 항목을 체크해주세요.",Toast.LENGTH_SHORT).show();
-                                                        }
-
+                                        if (checked) {
+                                                if (!check1.isChecked() && !check2.isChecked()) {
+                                                        check1.setChecked(true);
+                                                        check2.setChecked(true);
                                                 }
-                                        });
+                                        }
                                 }
+                        });
 
+                button.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                                try {
+                                        if (check1.isChecked() && check2.isChecked()) {
+                                                Intent intent = new Intent(Register_Auth.this, Register.class);
+                                                startActivity(intent);
+                                        } else if (check1.isChecked() && check2.isChecked() && checkBoxAll.isChecked()) {
+                                                Intent intent = new Intent(Register_Auth.this, Register.class);
+                                                startActivity(intent);
+                                        } else if(!check1.isChecked() && !check2.isChecked() && !checkBoxAll.isChecked()){
+                                                Toast.makeText(getApplicationContext(), "항목을 체크해주세요.", Toast.LENGTH_SHORT).show();
+                                                return;
+                                        } else {
+                                                Toast.makeText(getApplicationContext(), "필수 항목을 체크해주세요.", Toast.LENGTH_SHORT).show();
+                                                return;
+                                        }
+                                }catch (Exception e) {
+                                        e.printStackTrace();
+                                        Toast.makeText(getApplicationContext(), "시스템 오류입니다. 문의 부탁드립니다.", Toast.LENGTH_SHORT).show();
+                                        return;
+                                }
 
                         }
                 });
