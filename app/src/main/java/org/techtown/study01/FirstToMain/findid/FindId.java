cea0eac4c4e00c750f857da88ba68ec00601b782
Beaksooyeol <merrygoaround0726@gmail.com>
2021-02-12 14:05:06 +0900

    지메일 수정 전 저장 오늘처음

--- app/src/main/java/org/techtown/study01/FirstToMain/findid/FindId.java
+++ app/src/main/java/org/techtown/study01/FirstToMain/findid/FindId.java
@@ -35,6 +35,7 @@ public class FindId extends AppCompatActivity {
     private EditText editText;
     private Button button;
     private String Eid, email, Eemail; //찾은 아이디 넣을 스트링 객체
+    private Boolean emailSuccess, emailok = false;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -68,6 +69,8 @@ public class FindId extends AppCompatActivity {
                                 .create();
                         dialog.show();
                         return;
+                    } else{
+                        emailSuccess = true;
                     }
 
                 Response.Listener<String> responseListener = new Response.Listener<String>() {
@@ -81,7 +84,10 @@ public class FindId extends AppCompatActivity {
                             if (success) {
                                 Eemail = jsonObject.getString("email"); //찾는 아이디 값 가져오고
                                 Eid = jsonObject.getString("id"); //찾는 아이디 값 가져오고
-                                IdSendStart(email);
+                                if(emailSuccess == true){
+                                idSendStart();
+                                    emailok = true;
+                                }
                             } else {//로그인 실패시
                                 Toast.makeText(getApplicationContext(), "이메일을 다시 확인해주세요.", Toast.LENGTH_SHORT).show();
                                 return;
@@ -93,8 +99,6 @@ public class FindId extends AppCompatActivity {
                             return;
                         } catch (Exception e) {
                             e.printStackTrace();
-                            Toast.makeText(getApplicationContext(), "이메일 오류 2, 문의부탁드립니다.", Toast.LENGTH_SHORT).show();
-                            return;
                         }
                     }
                 };
@@ -108,18 +112,14 @@ public class FindId extends AppCompatActivity {
     // TODO: 2021-02-12 이거 해결하기
 
 
-    public void IdSendStart(String email) {
+    public void idSendStart() throws Exception {
         //구글 이메일로 smtp 사용해서 인증번호 보내기
         GMailSender gMailSender = new GMailSender("merrygoaround0726@gmail.com", "asdf4694");
                 //GMailSender.sendMail(제목, 본문내용, 받는사람);
-                try {
+
                     gMailSender.sendMail("금연 솔루션 플랫폼 '그만'입니다. 아이디를 확인해주세요.", "아이디는 \"" + Eid + "\" 입니다. \n " +
                             "아이디가 맞는지 확인해 주시고, 전체 아이디가 기억나시지 않으시면, 'merrygoaround0726@gmail.com' 이메일로 문의주시기 바랍니다. 감사합니다. ", email);
                     Toast.makeText(getApplicationContext(), "이메일로 아이디가 전송되었습니다.", Toast.LENGTH_SHORT).show();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    Toast.makeText(getApplicationContext(), "이메일 전송 오류, 문의 부탁드립니다.", Toast.LENGTH_SHORT).show();
-                }
-                return;
+
             }
 }
