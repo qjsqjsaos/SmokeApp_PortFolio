ce5cd713a0a7c7152427f6af0c36e123dd0310a2
Beaksooyeol <merrygoaround0726@gmail.com>
2021-02-18 00:10:11 +0900

    뷰페이저 하기 전 저장

--- app/src/main/java/org/techtown/study01/FirstToMain/homeMain/HomeMain.java
+++ app/src/main/java/org/techtown/study01/FirstToMain/homeMain/HomeMain.java
@@ -70,9 +70,6 @@ public class HomeMain extends Fragment {
 
 
 
-
-
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
